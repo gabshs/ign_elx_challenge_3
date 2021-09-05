1855cbb97c7d468c235b02bbe2f59678f24ec857
GabsDC <gabriel.silva@deliverycenter.com>
2021-09-05 16:28:44 -0300

    Creating filtered report

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -7,6 +7,12 @@ defmodule Flightex.Bookings.Report do
     |> File.write(handle_write())
   end
 
+  def generate_report(from_date, to_date) do
+    File.write("report-filtered.csv", handle_filter(from_date, to_date))
+
+    {:ok, "Report generated successfully"}
+  end
+
   defp handle_write do
     BookingAgent.list_all()
     |> Enum.map(&handle_line/1)
@@ -19,5 +25,14 @@ defmodule Flightex.Bookings.Report do
          user_id: user_id
        }) do
     "#{user_id}, #{local_origin}, #{local_destination}, #{complete_date}\n"
+  end
+
+  defp handle_filter(from_date, to_date) do
+    BookingAgent.list_all()
+    |> Enum.filter(fn %Booking{complete_date: date} ->
+      NaiveDateTime.compare(date, from_date) != :lt and
+        NaiveDateTime.compare(date, to_date) != :gt
+    end)
+    |> Enum.map(&handle_line/1)
   end
 end
--- test/bookings/report_test.exs
+++ test/bookings/report_test.exs
@@ -28,4 +28,46 @@ defmodule Flightex.Bookings.ReportTest do
       assert file =~ content
     end
   end
+
+  describe "generate_report/2" do
+    setup do
+      Flightex.start_agents()
+
+      :ok
+    end
+
+    test "when the booking is between dates filtered, return the content" do
+      params = %{
+        complete_date: ~N[2001-05-07 12:00:00],
+        local_origin: "Brasilia",
+        local_destination: "Bananeiras",
+        user_id: "12345678900",
+        id: UUID.uuid4()
+      }
+
+      content = "12345678900, Brasilia, Bananeiras, 2001-05-07 12:00:00"
+      Flightex.create_or_update_booking(params)
+      Report.generate_report(~N[2001-05-06 12:00:00], ~N[2021-05-07 12:00:00])
+      {:ok, file} = File.read("report-filtered.csv")
+
+      assert file =~ content
+    end
+
+    test "when the booking is not between dates filtered, return empty content" do
+      params = %{
+        complete_date: ~N[2001-05-07 12:00:00],
+        local_origin: "Brasilia",
+        local_destination: "Bananeiras",
+        user_id: "12345678900",
+        id: UUID.uuid4()
+      }
+
+      content = ""
+      Flightex.create_or_update_booking(params)
+      Report.generate_report(~N[2001-05-16 12:00:00], ~N[2021-05-07 12:00:00])
+      {:ok, file} = File.read("report-filtered.csv")
+
+      assert file == content
+    end
+  end
 end
